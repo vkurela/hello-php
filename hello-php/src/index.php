e5fe8ee2b5de55272beda049101d15a161d2d268
Venkat Kurela <31404074+vkurela@users.noreply.github.com>
2024-03-20 18:57:59 +0530

    Update index.php

--- hello-php/src/index.php
+++ hello-php/src/index.php
@@ -1,5 +1,5 @@
 <?php
-print ("<h2>Welcome to AWS ELB, this is after change </h2>");
+print ("<h2>Welcome to Docker Container LAB</h2>");
 print ("<h2> YOU ARE IN VIRTUAL CLASS ROOM WITH VENKAT KURELA </h2>");
 echo "You are connected to server IP::<br>";
 echo $_SERVER['SERVER_ADDR'];
